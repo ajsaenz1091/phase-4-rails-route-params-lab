9d690b0ac4b21940c1804ee33b4bfcc17c69f7a5
Alberto <ajsaenz1091@gmail.com>
2021-07-27 18:05:24 -0400

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+
+    name = params[:name]
+    students = if name 
+      Student.by_name(name)
+    else
+      Student.all
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,8 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def self.by_name(name)
+    students = self.arel_table
+    self.where((students[:first_name].matches("%#{name}%")).or(students[:last_name].matches("%#{name}%")))
+  end
 end
