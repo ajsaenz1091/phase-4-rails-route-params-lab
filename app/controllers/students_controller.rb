9f5f1a7adaf09b04df2593ccc746311e68a20cdc
Alberto <ajsaenz1091@gmail.com>
2021-07-27 17:19:46 -0400

    commit tree 17a843a52fed2c29084730b8619c1626f45e2b4d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    first_name = params[:name].split(" ").first
+    last_name = params[:name].split(" ").second
+    students = Student.where(first_name: first_name, last_name: last_name)
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
